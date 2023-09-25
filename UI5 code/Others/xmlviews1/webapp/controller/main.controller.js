6f14715c868e300781f5ba68fdf50fe6691cc532
SuryaTrainings <info@suryatrainings.com>
2023-09-25 01:36:22 +0000

    code updated

--- UI5 code/Others/xmlviews1/webapp/controller/main.controller.js
+++ UI5 code/Others/xmlviews1/webapp/controller/main.controller.js
@@ -1,7 +1,7 @@
 sap.ui.define([
     'sap/ui/core/mvc/Controller',
 ], function (oController) {
-    'use strict';
+    // 'use strict';
     console.log("Basic controller loaded successfully");
     return oController.extend("root.controller.main", {
 
@@ -11,11 +11,20 @@ sap.ui.define([
 
             console.log("Method loaded successfully");
         },
-        onclick: function(oEvent){
-            oDocument = sap.ui.getCore();
-            var oInp = oDocument.byId("idInp");
-            var sVal = oInp.getValue();
-            alert(sVal);
+        onClick: function(oEvent){
+            //Code - 1 
+            alert("Tested");
+
+            //Code - 2 
+            // oDocument = sap.ui.getCore();
+            // var oInp = oDocument.byId("idMain--idInput");
+            // var sVal = oInp.getValue();
+            // alert(sVal);
+
+            //code -3 
+            var oDocument = this.getView();
+            var oInp = oDocument.byId("idInput");
+            alert(oInp.getValue());
         }
     });
 });
