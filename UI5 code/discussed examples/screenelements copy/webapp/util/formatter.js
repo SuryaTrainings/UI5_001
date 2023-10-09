515b8431b707bb1e4451bf424e957673e9ded9f9
SuryaTrainings <info@suryatrainings.com>
2023-10-09 02:02:06 +0000

    code update

--- UI5 code/discussed examples/screenelements copy/webapp/util/formatter.js
+++ UI5 code/discussed examples/screenelements copy/webapp/util/formatter.js
@@ -1,15 +1,15 @@
 sap.ui.define([
     "sap/ui/core/format/NumberFormat"
-], function(oNumberFormat) {
+], function (oNumberFormat) {
     'use strict';
     return {
-        convert2UpperCase: function(oInput){
+        convert2UpperCase: function (oInput) {
             debugger;
-            if(oInput)
-            {
-            return oInput.toUpperCase(); }
+            if (oInput) {
+                return oInput.toUpperCase();
+            }
         },
-        toFormattedAmount: function(input1, input2){
+        toFormattedAmount: function (input1, input2) {
             debugger;
             var oCurrencyFormat = oNumberFormat.getCurrencyInstance();
             return oCurrencyFormat.format(input1, input2);
