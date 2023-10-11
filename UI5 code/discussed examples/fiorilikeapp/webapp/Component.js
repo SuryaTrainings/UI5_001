9999208f8fd22d2a994cdb43f316792a67ed37f9
SuryaTrainings <info@suryatrainings.com>
2023-10-11 06:14:27 +0000

    code updated

---
+++ UI5 code/discussed examples/fiorilikeapp/webapp/Component.js
@@ -0,0 +1,30 @@
+sap.ui.define([
+    'sap.ui.core.UIComponent'
+], function(UIComponent) {
+    'use strict';
+
+    return UIComponent.extend("st.b33.simpleApp.Component",{
+        //1 - metadata 
+        metadata:{
+            manifest: "json"
+        },
+        //2 - init ( key ) - Prototype 
+        init:function(){
+        sap.ui.core.UIComponent.prototype.init.apply(this);
+        },
+        //3 - Load our own data 
+        createContent: function(){
+            var oAppView = new sap.ui.view("idMain",{
+                viewName: "st.b33.simpleApp.view.layout",
+                type: sap.ui.core.mvc.ViewType.XML
+            });
+
+            return oAppView;
+        },
+        //4 - destory 
+        destroy: function(){
+
+        }
+    })
+    
+});
