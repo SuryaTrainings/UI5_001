688d5c9adbd641ecb07c4d46af9dbff89264f7d0
SuryaTrainings <info@suryatrainings.com>
2023-10-18 05:28:48 +0000

    code updated

--- UI5 code/exercises/dynamicFragments/webapp/controller/view2.controller.js
+++ UI5 code/exercises/dynamicFragments/webapp/controller/view2.controller.js
@@ -1,45 +1,45 @@
 sap.ui.define([
     'st/b33/SimpleApp/controller/BaseController',
     'sap/ui/core/Fragment'
-], function(BaseController,Fragment) {
+], function (BaseController, Fragment) {
     'use strict';
-    return BaseController.extend("st.b33.SimpleApp.controller.view2",{
+    return BaseController.extend("st.b33.SimpleApp.controller.view2", {
 
-        onBack:function(){
-            var oView=this.getView();
-            var oAppcon=oView.getParent();
+        onBack: function () {
+            var oView = this.getView();
+            var oAppcon = oView.getParent();
             oAppcon.to("idView1");
         },
 
-       getValueRequest:function(){
-            
+        getValueRequest: function () {
+
             //alert("popup working successfully");
             this.onFilter();
         },
 
-        supplierpopup:null,
-onFilter:function(){
-//alert("popup working successfully");
-var that;
-that=this;
+        supplierpopup: null,
+        onFilter: function () {
+            //alert("popup working successfully");
+            var that;
+            that = this;
 
-if(!this.supplierpopup ){
-            Fragment.load({
-                id:"supplier",
-                name: "st.b33.SimpleApp.fragments.popup",
-                controller: this
-            }).then(function(oFragment){
-                that.supplierpopup=oFragment;
-               that.supplierpopup.open();
-            });
-        }else{
-            that.supplierpopup.open();
+            if (!this.supplierpopup) {
+                Fragment.load({
+                    id: "supplier",
+                    name: "st.b33.SimpleApp.fragments.popup",
+                    controller: this
+                }).then(function (oFragment) {
+                    that.supplierpopup = oFragment;
+                    that.supplierpopup.open();
+                });
+            } else {
+                that.supplierpopup.open();
+            }
+
+
+
         }
-               
-            
-           
-        }
-       
+
 
     })
 });
