929167df0d989c5f777aa74976e5b55a771e24d2
SuryaTrainings <info@suryatrainings.com>
2023-10-18 04:47:49 +0000

    code updated

---
+++ UI5 code/discussed examples/dynamicfragments/webapp/controller/view2.controller.js
@@ -0,0 +1,101 @@
+sap.ui.define([
+    'st/b33/simpleApp/controller/BaseController',
+    'st/b33/simpleApp/util/formatter',
+    'sap/ui/core/Fragment'
+
+], function (BaseController, Formatter, Fragment) {
+    'use strict';
+    return BaseController.extend("st.b33.simpleApp.controller.view2", {
+        oFormatter: Formatter,
+        onBack: function () {
+            var oView = this.getView();
+            var oAppCon = oView.getParent();
+
+            // Navigate to child object
+            //Get child 2 id name 
+            oAppCon.to("idView1");
+        },
+
+
+        citiesPopUp: null,
+        oCitiName: null,
+
+        oValueHelp: function (oEvent) {
+            debugger;
+            var that;
+
+            that = this;
+
+            this.oCitiName = oEvent.getSource();
+
+            if (!this.citiesPopUp) {
+
+                Fragment.load({
+                    id: "cities",
+                    name: "st.b33.simpleApp.fragments.popup",
+                    controller: this
+                }).then(function (oFragment) {
+                    that.citiesPopUp = oFragment;
+                    that.citiesPopUp.setTitle("Cities popup");
+                    that.citiesPopUp.setMultiSelect(false);
+                    that.getView().addDependent(that.citiesPopUp);
+                    //Connect model to the fragment 
+                    that.citiesPopUp.bindAggregation("items", {
+                        //This is absoulte path 
+                        path: "/Cities",
+                        //Item data with relative path 
+                        template: new sap.m.DisplayListItem({
+                            label: "{CityName}",
+                            value: "{FamousFor}"
+                        })
+                    });
+                    that.citiesPopUp.open();
+                });
+            } else {
+                this.citiesPopUp.open();
+            }
+        },
+
+        supplierPopUp: null,
+
+        onFilter: function () {
+            var that;
+            that = this;
+            if (!this.supplierPopUp) {
+                Fragment.load({
+                    id: "supplier",
+                    name: "st.b33.simpleApp.fragments.popup",
+                    controller: this
+                }).then(function (oFragment) {
+                    //Object Created
+                    that.supplierPopUp = oFragment;
+                    //Set title
+                    that.supplierPopUp.setTitle("Supplier popup");
+                    //Saying to view this fragment is dependent to display my data
+                    that.getView().addDependent(that.supplierPopUp);
+                    //Connect model to the fragment 
+                    that.supplierPopUp.bindAggregation("items", {
+                        //This is absoulte path 
+                        path: "/Suppliers",
+                        //Item data with relative path 
+                        template: new sap.m.DisplayListItem({
+                            label: "{name}",
+                            value: "{sinceWhen}"
+                        })
+                    });
+                    that.supplierPopUp.open();
+                });
+            } else {
+                this.supplierPopUp.open();
+            }
+        },
+        onPopupConfirm: function (oEvent) {
+            var oWhichItemIsSelected = oEvent.getParameter("selectedItem");
+            debugger;
+            var oSelectedCitiName = oWhichItemIsSelected.getLabel();
+            if (oEvent.getSource().getId().indexOf("supplier") != -1) {
+                this.oCitiName.setValue(oSelectedCitiName);
+            }
+        }
+    })
+});
