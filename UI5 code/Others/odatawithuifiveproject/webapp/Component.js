0dd1d20c12ba65186866e0b7d02ecd23aae3d78f
SuryaTrainings <info@suryatrainings.com>
2023-11-09 01:05:29 +0000

    code updated

---
+++ UI5 code/Others/odatawithuifiveproject/webapp/Component.js
@@ -0,0 +1,50 @@
+sap.ui.define([
+    'sap/ui/core/UIComponent'
+], function(UIComponent) {
+    'use strict';
+
+    return UIComponent.extend("st.b33.simpleApp.Component",{
+        //1 - metadata 
+        metadata:{
+            manifest: "json"
+        },
+        //2 - init ( key ) - Prototype 
+        init:function(){
+            sap.ui.core.UIComponent.prototype.init.apply(this);
+
+            //Get the router object
+            var oRouter = this.getRouter();
+            oRouter.initialize();    
+        },
+        //3 - Load our own data 
+        // createContent: function(){
+        //     var oAppView = new sap.ui.view("idMain",{
+        //         viewName: "st.b33.simpleApp.view.App",
+        //         type: sap.ui.core.mvc.ViewType.XML
+        //     });
+
+
+        //     var oAppView1 = new sap.ui.view("idView1",{
+        //         viewName: "st.b33.simpleApp.view.view1",
+        //         type: sap.ui.core.mvc.ViewType.XML
+        //     });
+
+            
+        //     var oAppView2 = new sap.ui.view("idView2",{
+        //         viewName: "st.b33.simpleApp.view.view2",
+        //         type: sap.ui.core.mvc.ViewType.XML
+        //     });
+
+        //     var oAppCon = oAppView.byId("idAppCon");
+        //     oAppCon.addMasterPage(oAppView1);
+        //     oAppCon.addDetailPage(oAppView2);
+
+        //     return oAppView;
+        // },
+        //4 - destory 
+        destroy: function(){
+
+        }
+    })
+    
+});
---
+++ UI5 code/Others/odatawithuifiveproject/webapp/controller/view3.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    'st/b33/simpleApp/controller/BaseController',
+    'st/b33/simpleApp/util/formatter',
+    'sap/ui/core/Fragment'
+
+], function (BaseController, Formatter, Fragment) {
+    'use strict';
+    return BaseController.extend("st.b33.simpleApp.controller.view2", {
+        
+    })
+});
