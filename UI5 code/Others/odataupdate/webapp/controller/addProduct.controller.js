fd82d9094d2c77493d7424af60328a3874861fb9
SuryaTrainings <info@suryatrainings.com>
2023-11-14 01:35:28 +0000

    code updated

--- UI5 code/Others/odataupdate/webapp/controller/addProduct.controller.js
+++ UI5 code/Others/odataupdate/webapp/controller/addProduct.controller.js
@@ -7,7 +7,6 @@ sap.ui.define([
 ], function (BaseController, JSONModel, MessageToast, MessageBox) {
     'use strict';
     return BaseController.extend("st.b33.simpleApp.controller.addProduct", {
-        productPath: '',
         onInit: function () {
 
             var oJsonModel = new JSONModel();
@@ -56,7 +55,7 @@ sap.ui.define([
                 "ProductSet": {
                     "PRODUCT_ID": "",
                     "TYPE_CODE": "PR",
-                    "CATEGORY": "Notebooks",
+                    "CATEGORY": "",
                     "NAME": "",
                     "DESCRIPTION": "",
                     "SUPPLIER_ID": "0100000049",
@@ -72,92 +71,24 @@ sap.ui.define([
         onSubmit: function(oEvent){
             //1 - Get product ID entered by user on screen.
             var oPrdId = oEvent.getParameter("value");
-            var that = this;
+
             //2 - Get OData model object & JSON Model
             var oJsonModel = this.getView().getModel('prod');
             var oDataModel = this.getView().getModel();
 
             //3 - Prepare path to read odata object
             var sPath = "/ProductSet('"+oPrdId+"')";
-            this.productPath = sPath;
             //4 - Fire odata get call to read
             oDataModel.read(sPath, {
                 success: function(payload){
                     oJsonModel.setProperty("/ProductSet",payload);
                     MessageToast.show(sPath+" Product loaded successfully");
-                    debugger;
-                    that.loadPhoto(sPath).bind(that);
                 },
                 error: function(oErrorResult){
                     MessageBox.error(JSON.parse(oErrorResult.responseText).error.innererror.errordetails[0].message);
                 }
             })
             //5 - Set data back to view
-        },
-        onCostlyProduct: function(){
-            //1 - Get the category from Dropdown
-            var sPayload = this.getView().getModel("prod").getProperty("/ProductSet");
-            var sCategory = sPayload.CATEGORY;
-            var that = this;
-            //2 - Get OData model object
-            var oDataModel = this.getView().getModel();
-
-            //3 - Call odata request to load data from server 
-            oDataModel.callFunction("/GetMostExpensiveProduct",{
-                urlParameters: {
-                    "I_CATEGORY": sCategory
-                },
-                success: function(oSuccess){
-                    that.getView().getModel("prod").setProperty("/ProductSet",oSuccess);
-                },
-                error: function(oErrorResult){
-                    MessageBox.error(JSON.parse(oErrorResult.responseText).error.innererror.errordetails[0].message);
-                }
-            });
-            //4 - Call back
-            //5 - Set data to screen
-
-        },
-        loadPhoto: function(sPath){
-            sPath = sPath.replace("ProductSet","ProdPicSet");
-            var oPhoto = "/sap/opu/odata/SAP/ZUI_ODATA_01_SRV" + sPath + "/$value";
-            this.getView().byId("idPhoto").setSrc("oPhoto");
-        },
-        onDelete: function(){
-            var that = this;
-            MessageBox.confirm("Would you like to delete?",{
-                onClose: function(status){
-                    if( status === "OK"){
-                        //step - 2 : Get odata model object
-                        var oDataModel = that.getView().getModel();
-                        //Step - 3 : Send delete request
-                        oDataModel.remove(that.productPath,{
-                            success : function(){
-                                MessageToast.show("Product deleted successfully");
-                                that.getView().getModel('prod').setData({
-                                    "ProductSet": {
-                                        "PRODUCT_ID": "",
-                                        "TYPE_CODE": "PR",
-                                        "CATEGORY": "",
-                                        "NAME": "",
-                                        "DESCRIPTION": "",
-                                        "SUPPLIER_ID": "0100000049",
-                                        "SUPPLIER_NAME": "Talpa",
-                                        "WEIGHT_MEASURE": "4.200 ",
-                                        "WEIGHT_UNIT": "KG",
-                                        "PRICE": "0.00",
-                                        "CURRENCY_CODE": ""
-                                    }});
-                            },
-                            error: function(){
-                                MessageToast.show("Product deleteion failed");
-                            }
-                        })
-                        //Step - 4 : Handle call back
-
-                    }
-                }
-            })
         }
     })
 });
---
+++ UI5 code/Others/odataupdate/webapp/controller/view1.controller.js
@@ -0,0 +1,97 @@
+sap.ui.define([
+    'st/b33/simpleApp/controller/BaseController',
+    'sap/ui/model/Filter',
+    'sap/ui/model/FilterOperator',
+    'st/b33/simpleApp/util/formatter'
+], function(BaseController,Filter,FilterOperator,Formatter) {
+    'use strict';
+
+    return BaseController.extend("st.b33.simpleApp.controller.view1",{
+
+        onInit: function(){
+           this.oRouter = this.getOwnerComponent().getRouter();
+        },
+        onPress: function(){
+            this.oRouter.navTo("addProductData");
+        },
+        onNext: function(oSpath){
+            // // get parent object
+            // var oView = this.getView();
+            // var oAppCon = oView.getParent();
+
+            // // Navigate to child object
+            // //Get child 2 id name 
+            // oAppCon.to("idView2");
+
+            this.oRouter.navTo("productData",{
+                pid: oSpath.replace("/","")
+            })
+        },
+        // onSerach: function(oEvent){
+        //     debugger;
+
+        //  //Get Item data
+        //  var oItemData = this.getView().byId("idListData").getBinding("items");
+         
+        //  //Get Query String
+        //  var oSearchInput = oEvent.getParameter("query");
+
+        //  //Create Filter 
+        //  var oFilterName = new Filter("name", FilterOperator.Contains, oSearchInput );
+        //  var oFilterType = new Filter("type", FilterOperator.Contains, oSearchInput );
+        //  var oFilter = new Filter({
+        //     filters: [oFilterName, oFilterType],
+        //     and: false
+        //  });
+         onSerach: function(oEvent){
+            debugger;
+
+         //Get Item data
+         var oItemData = this.getView().byId("idListData").getBinding("items");
+         
+         //Get Query String
+         var oSearchInput = oEvent.getParameter("newValue");
+
+         //Create Filter 
+         var oFilterName = new Filter("CATEGORY", FilterOperator.Contains, oSearchInput );
+         var oFilterType = new Filter("type", FilterOperator.Contains, oSearchInput );
+         var oFilter = new Filter({
+            filters: [oFilterName]
+            //filters: [oFilterName, oFilterType],
+            //and: false
+         });
+
+        //Apply filter on dataset
+        oItemData.filter([oFilter]);
+        },
+
+        onDelete: function(oEvent){
+            // Get the selected item
+            var oSeletedItem = oEvent.getParameter("listItem"); 
+            // Get source / list parent 
+            var oList = oEvent.getSource();
+            // Remove item from the parent 
+            oList.removeItem(oSeletedItem);
+        },
+        onSelectionChange: function(oEvent){
+
+            //Get the selected item
+            var oSelectedItem = oEvent.getParameter("listItem");
+
+            //Extract the item index.
+            var oSpath = oSelectedItem.getBindingContextPath();
+
+            debugger;
+
+            //Bind the data to view 2. - Aginest to the polity 
+            //var oView2 = this.getView().getParent().getParent().getDetailPages()[0];
+
+            //Get view2 refernce             
+            // oView2.bindElement(oSpath, {
+            //     expand: 'To_Supplier' }
+            //     );
+
+            this.onNext(oSpath);
+        }
+    })
+});
