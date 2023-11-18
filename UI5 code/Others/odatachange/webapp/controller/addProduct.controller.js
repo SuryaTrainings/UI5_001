90e026accc1cdfe3694ba784c443162e0e907aee
SuryaTrainings <info@suryatrainings.com>
2023-11-18 02:47:47 +0000

    code update

--- UI5 code/Others/odatachange/webapp/controller/addProduct.controller.js
+++ UI5 code/Others/odatachange/webapp/controller/addProduct.controller.js
@@ -3,87 +3,96 @@ sap.ui.define([
     'sap/ui/model/json/JSONModel',
     'sap/m/MessageToast',
     'sap/m/MessageBox'
-], function(BaseController,JSONModel,MessageToast,MessageBox) {
+], function (BaseController, JSONModel, MessageToast, MessageBox) {
     'use strict';
-    return BaseController.extend("st.b33.simpleApp.controller.addProduct",{
-       
-        onInit: function(){
+    return BaseController.extend("st.b33.simpleApp.controller.addProduct", {
+
+        onInit: function () {
             var oJSONModel = new JSONModel();
-           
+
             oJSONModel.setData({
 
-                ProductSet : {
-                    "PRODUCT_ID" : "HT-123",
-                    "TYPE_CODE" : "PR",
-                    "CATEGORY" : "Notebooks",
-                    "NAME" : "Science NoteBooks",
-                    "DESCRIPTION" : "Notebooks are very useful for Writing purpose",
-                    "SUPPLIER_ID" : "0100000046",
-                    "SUPPLIER_NAME" : "SAP",
-                    "WEIGHT_MEASURE" : "4.200 ",
-                    "MEASURE_UNIT" : "EA",
-                   
+                ProductSet: {
+                    "PRODUCT_ID": "HT-123",
+                    "TYPE_CODE": "PR",
+                    "CATEGORY": "Notebooks",
+                    "NAME": "Science NoteBooks",
+                    "DESCRIPTION": "Notebooks are very useful for Writing purpose",
+                    "SUPPLIER_ID": "0100000046",
+                    "SUPPLIER_NAME": "SAP",
+                    "WEIGHT_MEASURE": "4.200 ",
+                    "MEASURE_UNIT": "EA",
+
                 }
 
-
             });
-            this.getView().setModel(oJSONModel,"prod");
-
-    
-
+            this.getView().setModel(oJSONModel, "prod");
+            this.getOwnerComponent().getRouter().getRoute("addProductData").attachMatched(this.setMode, this);
         },
 
-        onReset: function(){
+        setMode: function () {
+            this.changeMode("Create");
+        },
+        sPath : "" ,
+
+        onReset: function () {
+
+            this.changeMode("Create");
+
             this.getView().getModel("prod").setData({
+                "enable": true,
+                ProductSet: {
+                    "PRODUCT_ID": "",
+                    "TYPE_CODE": "PR",
+                    "CATEGORY": "",
+                    "NAME": "",
+                    "DESCRIPTION": "",
+                    "SUPPLIER_ID": "0100000046",
+                    "SUPPLIER_NAME": "SAP",
+                    "WEIGHT_MEASURE": "4.200 ",
+                    "MEASURE_UNIT": "EA",
 
-                ProductSet : {
-                    "PRODUCT_ID" : "",
-                    "TYPE_CODE" : "PR",
-                    "CATEGORY" : "",
-                    "NAME" : "",
-                    "DESCRIPTION" : "",
-                    "SUPPLIER_ID" : "0100000046",
-                    "SUPPLIER_NAME" : "SAP",
-                    "WEIGHT_MEASURE" : "4.200 ",
-                    "MEASURE_UNIT" : "EA",
-                   
                 }
 
             });
         },
 
-        onGet: function(){
+        onGet: function () {
+
+            var that = this;
+
             debugger;
             //1.Read product Id
-          var oinput = this.getView().byId("idProductId").getValue();
+            var oinput = this.getView().byId("idProductID").getValue();
             //2.Get OData model,Get JSON MODEL.
             var oDataModel = this.getView().getModel();
             var oJsonModel = this.getView().getModel("prod");
-           //3.ProductSet('HT-1000')
-           var sPath = "/ProductSet('" + oinput + "')";
-
+            //3.ProductSet('HT-1000')
+            var sPath = "/ProductSet('" + oinput + "')";
+            this.sPath = sPath;
             //4.Perform read operations via Odata Call
-            oDataModel.read(sPath,{
-                success: function(data){
-                    oJsonModel.setProperty("/ProductSet",data);
+            oDataModel.read(sPath, {
+                success: function (data) {
+                    oJsonModel.setProperty("/ProductSet", data);
                     MessageBox.success("Data loaded successfully");
+                    that.changeMode("Change");
                 },
-                error: function(ErrorMsg){
+                error: function (ErrorMsg) {
                     MessageBox.error(JSON.parse(ErrorMsg.responseText).error.innererror.errordetails[0].message);
 
                 }
-     });
-     var sImagePath = 
-     '/sap/opu/odata/SAP/ZUI_ODATA_02_SRV/' +
-     "MediaSet('"+ 
-     oinput + 
-     "')/$value";
-     this.getView().byId("idImageProduct").setSrc(sImagePath);
+            });
+            var sImagePath =
+                '/sap/opu/odata/SAP/ZUI_ODATA_02_SRV/' +
+                "MediaSet('" +
+                oinput +
+                "')/$value";
+            //this.getView().byId("idImageProduct").setSrc(sImagePath);
 
             //5.Success-bind data to ojson model
         },
 
-        onDelete: function(){
+        onDelete: function () {
             debugger;
             var that = this;
             //1.Get selected productid
@@ -96,29 +105,29 @@ sap.ui.define([
             var sPath = "/ProductSet('" + oinput + "')";
 
             //5.Call remove request
-            oDataModel.remove( sPath,{
+            oDataModel.remove(sPath, {
                 success: function (data) {
                     MessageBox.success("Data deleted successfully");
                     that.getView().getModel("prod").setData({
+                        "enable": true,
+                        "ProductSet": {
+                            "PRODUCT_ID": "",
+                            "TYPE_CODE": "PR",
+                            "CATEGORY": "",
+                            "NAME": "",
+                            "DESCRIPTION": "",
+                            "SUPPLIER_ID": "0100000046",
+                            "SUPPLIER_NAME": "SAP",
+                            "WEIGHT_MEASURE": "4.200 ",
+                            "MEASURE_UNIT": "EA",
 
-                        ProductSet : {
-                            "PRODUCT_ID" : "",
-                            "TYPE_CODE" : "PR",
-                            "CATEGORY" : "",
-                            "NAME" : "",
-                            "DESCRIPTION" : "",
-                            "SUPPLIER_ID" : "0100000046",
-                            "SUPPLIER_NAME" : "SAP",
-                            "WEIGHT_MEASURE" : "4.200 ",
-                            "MEASURE_UNIT" : "EA",
-                           
                         }
-        
+
                     });
 
 
                 },
-                error: function(ErrorMsg){
+                error: function (ErrorMsg) {
                     MessageBox.error(JSON.parse(ErrorMsg.responseText).error.innererror.errordetails[0].message);
 
 
@@ -131,39 +140,58 @@ sap.ui.define([
 
         },
 
+        mode: "create",
 
-
-
-
-
-
-
-
-
-
-        onSave: function(){
+        changeMode: function (sMode) {
+            if (sMode === "Create") {
+                this.mode = "Create";
+                this.getView().byId("idSaveData").setText("Create");
+                this.getView().getModel("prod").setProperty("/enable", true);
+            } else {
+                this.mode = "Change";
+                this.getView().byId("idSaveData").setText("Update");
+                this.getView().getModel("prod").setProperty("/enable", false);
+            }
+        },
+        onSave: function () {
 
             debugger;
 
             var payload = this.getView().getModel("prod").getProperty("/ProductSet");
             //get Default model odata model:manifest.json
             var oDataModel = this.getView().getModel();
+
+            if(this.mode === "Create"){
             //post data to OData services with create method
-            oDataModel.create('/ProductSet', payload ,{
-                success: function(){
+            oDataModel.create('/ProductSet', payload, {
+                success: function () {
                     MessageBox.success("Data loaded successfully");
 
                 },
-                error: function(ErrorMsg){
-                   // MessageToast.show("Data not created successfully");
-                   MessageBox.error(JSON.parse(ErrorMsg.responseText).error.innererror.errordetails[0].message);
+                error: function (ErrorMsg) {
+                    // MessageToast.show("Data not created successfully");
+                    MessageBox.error(JSON.parse(ErrorMsg.responseText).error.innererror.errordetails[0].message);
 
                 }
 
-            }) 
+            })}
+            else{
+                oDataModel.update(this.sPath, payload, {
+                    success: function () {
+                        MessageBox.success("Data updated successfully");
+    
+                    },
+                    error: function (ErrorMsg) {
+                        // MessageToast.show("Data not updated successfully");
+                        MessageBox.error(JSON.parse(ErrorMsg.responseText).error.innererror.errordetails[0].message);
+    
+                    }
+    
+                })
+            }
 
 
         }
-        
+
     })
 });
